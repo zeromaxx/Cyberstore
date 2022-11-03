6103331b91cfd6b42f2d05f465c182f68e9d78b5
Aris Lamprinidis <zeromixer2010@yahoo.com>
2022-11-03 21:15:03 +0200

    fixed modals

--- Eshop/App_Start/RouteConfig.cs
+++ Eshop/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Eshop
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "User", action = "Login", id = UrlParameter.Optional }
             );
 
           //  routes.MapRoute(
