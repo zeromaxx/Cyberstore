9e87ccf4716e152bbe70d4ae0715ebd85ab1a459
Aris Lamprinidis <zeromixer2010@yahoo.com>
2022-11-06 15:03:36 +0200

    hope for the best

---
+++ Startup.cs
@@ -0,0 +1,18 @@
+﻿
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+
+namespace Eshop
+{
+    public class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+
+            ConfigureAuth(app);
+        }
+
+    }
+}
